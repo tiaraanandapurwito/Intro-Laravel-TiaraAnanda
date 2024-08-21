7aeca591728c9972f321b25df5d9f4dc6c95bc1a
tiaraanandapurwito <tiarapurwito23@gmail.com>
2024-08-21 16:24:20 +0700

    update tugas hari ke 13

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\TableController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/home', [HomeController::class, 'home']);
@@ -9,3 +10,6 @@ Route::get('/home', [HomeController::class, 'home']);
 Route::get('/register', [AuthController::class, 'register']);
 
 Route::get('/welcome', [AuthController::class, 'welcome']);
+
+Route::get('/table', [TableController::class, 'table']);
+Route::get('/datatable', [TableController::class, 'datatable']);
